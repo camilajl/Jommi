aae8c9b78d86a5642b207bcaee25c8033e02b215
jesus garcia <jesus.garcia@venturatravel.org>
2024-06-17 17:06:38 -0500

    new fix loading and error

--- src/components/Pages/App/Users/index.tsx
+++ src/components/Pages/App/Users/index.tsx
@@ -22,7 +22,7 @@ const UsersPage = () => {
           value={search}
           placeholder='Buscar usuario'
           onChange={(e) => setSearch(e.target.value)}
-          onClick={() => { }}
+          onClick={() => {}}
         />
       </div>
 
