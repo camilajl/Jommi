f887a3c3cf9eb20c63be7e11ced17cd7d2adfba7
jesus garcia <jesusgarciab3aa@hotmail.com>
2024-08-30 18:23:38 -0500

    prepare for login change

--- src/routing/SignIn.tsx
+++ src/routing/SignIn.tsx
@@ -3,6 +3,7 @@ import { signIn } from 'next-auth/react';
 import LoadingCustom from '@/src/components/UI/Loading/Loading';
 import { useEffect } from 'react';
 const SignIn = () => {
+  //change component to normal login page without auth0
   useEffect(() => {
     signIn('auth0');
   }, []);
